3e7d2c5e88f28bae6285be5ba85d5d98bfdf84bd
Choi Dong Bin <43774570+DBChoi85@users.noreply.github.com>
2026-06-18 14:53:26 +0900

    Update utils.py

--- myapp/utils.py
+++ myapp/utils.py
@@ -1,9 +1,9 @@
 BASE_URL = 'https://www.daegu.go.kr/daeguchain/v2/mitum'
-API_TOKEN = 'a0d97bbec18699bb941186c1301f2d0f'
+API_TOKEN = ''
 CHAIN_NAME = 'dchain'
 HEADERS = {
     'Content-Type': 'application/json',
     'Accept' : 'application/json'
     }
 OWNER_ADDR = '0x55b0e6aF0182306C3188631dB57269a50171f480fca'
-OWNER_PRIVATE = '537756597539627272b62f7b4a4431eae2b07d036360ab54590a0ea20211bf4efpr'
+OWNER_PRIVATE = ''
